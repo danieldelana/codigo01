993d8e0f567b7d17ebb9e6b0d7ef09c8d5be0e73
danieldelana <danieldelana@hotmail.com>
2020-11-30 18:47:41 -0400

    loop v0.5

--- loop.js
+++ loop.js
@@ -10,8 +10,19 @@ var itens2=[
 console.log(Number(Math.random()*itens2));
 // sem retorno esperado...
 
-for(a of itens2){
-    b=Math.random()*a
-    console.log(Math.round(b));
-}
+// for(a of itens2){
+//     b=Math.random()*a
+//     console.log(Math.round(b));
+// }
 //com math.round sendo usado depois de se gerar uma nova variavel... sucesso
+    var x=999
+    for(i=1; i<6;i++){             
+      a=Math.round(Math.random()*x)
+      b=Math.round(Math.random()*x)
+      if(b>100) {b=Math.round(Math.random()*100)
+      console.log(b)}
+        else if(b<100){b=Math.round(Math.random()*x)
+        console.log(b)}
+        else if(a>100) {a=NaN
+        console.log(a)};
+    }
